dc78079e1390eab463fa82182f7b81ec49c4e2ab
Raaga Pranitha Kolla <raagapranithakolla@Raagas-MacBook-Pro.local>
2021-01-15 15:41:26 -0800

    Add comments

--- project/actions/actions.py
+++ project/actions/actions.py
@@ -1,31 +1,10 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
-
-
-# This is a simple example for a custom action which utters "Hello World!"
-
 from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from database_connection import dataUpdate
 
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+
 class ActionSubmit(Action):
 
     def name(self) -> Text:
@@ -35,7 +14,7 @@ class ActionSubmit(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        # dispatcher.utter_message(text="Your Name is {0}\n and your address is {1}\n your phone number is {2}\n your requested items are {3}\n".format(tracker.get_slot("name"),tracker.get_slot("address"),tracker.get_slot("mobile"),tracker.get_slot("items")))
+        # Calls dataUpdate function from database_connection.py and sends the slots values it collected from chatbot
         dataUpdate(tracker.get_slot("name"),tracker.get_slot("address"),tracker.get_slot("zipcode"),tracker.get_slot("mobile"),tracker.get_slot("items"))
 
         return []
--- project/database_connection.py
+++ project/database_connection.py
@@ -2,22 +2,21 @@ import mysql.connector
 
 
 def dataUpdate(name,address,zipcode,mobile,items):
-
+'''
+Connects to database and inserts the record
+'''
 	mydb = mysql.connector.connect(
 	  host="localhost",
 	  user="root",
-	  password="anil1234",
+	  password="",
 	  database='chatbot_db'
 	)
 
 	mycursor=mydb.cursor()
+	#Inserts record into requests table
 	sql = "INSERT INTO requests (Name, Address, Zipcode, Mobile, Items, Status) VALUES (%s, %s, %s, %s, %s, %s);"
 	val = (name, address, zipcode, mobile, items, 'Active')
 	mycursor.execute(sql, val)
-	# sql=f'INSERT INTO requests (Name, Address, Mobile_num, Request, Status) values ({name},{address},{mobile},{items},'Active');'
-	# mycursor.execute(sql)
 	mydb.commit()
 	print(mycursor.rowcount,'record inserted')
 
-# if __name__=='__main__':
-# 	dataUpdate('Test_name_python','test_address_python','test_mobile_python','test_items_python')
